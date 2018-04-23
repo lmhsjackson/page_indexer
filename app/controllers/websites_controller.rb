bf5eade95741a24fcfd23a40a0582a858ce8e623
Jose Piccioni <josepiccioni@gmail.com>
2018-04-23 13:48:03 -0300

    Add url validator on websites controller

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -1,5 +1,14 @@
+require 'uri'
+
 class WebsitesController < ApplicationController
-  # def create
-  #
-  # end
+  before_action :valid_url?, :only => :create
+
+#  def create
+#
+#  end
+
+  def valid_url?(url)
+    uri = URI.parse(url)
+    uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+  end
 end
--- spec/controllers/websites_controller_spec.rb
+++ spec/controllers/websites_controller_spec.rb
@@ -1,5 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe WebsitesController, type: :controller do
+  describe 'validate_url' do
+    it 'returns false if http/https is missing' do
+      valid = subject.valid_url? 'www.google.com'
+      expect(valid).to be_falsey
+    end
 
+    it 'returns false if the string is garbage' do
+      valid = subject.valid_url? 'tomato'
+      expect(valid).to be_falsey
+    end
+
+    it 'returns true if the url is valid' do
+      valid = subject.valid_url? 'http://www.google.com'
+      expect(valid).to be_truthy
+    end
+  end
 end
