37f9ae599db3a22c7d918d69f3cf1867c2d16936
Jose Piccioni <josepiccioni@gmail.com>
2018-04-24 14:11:40 -0300

    Add custom create method in websites controller.
      Simple exception handling

--- app/controllers/websites_controller.rb
+++ app/controllers/websites_controller.rb
@@ -1,14 +1,29 @@
 require 'uri'
 
 class WebsitesController < ApplicationController
-  before_action :valid_url?, :only => :create
+  before_action :valid_url?, only: :create
 
-#  def create
-#
-#  end
+  def create
+    Website.create(url: url).index
+    render :json => { ulr: "#{url} was created successfully"}
+  rescue Errno::ENOENT
+    render :json => { error: "Your url is probably malformed (missing http://?) url: #{url}" }, status: 400
+  rescue SocketError => e
+    render :json => { error: "The following error occured: #{e.message}" }, status: 404
+  end
 
-  def valid_url?(url)
+  def valid_url?
     uri = URI.parse(url)
     uri.is_a?(URI::HTTP) || uri.is_a?(URI::HTTPS)
+  end
+
+  private
+
+  def url
+    @url ||= website_params['attributes']["url"]
+  end
+
+  def website_params
+    params.require(:data).permit!
   end
 end
