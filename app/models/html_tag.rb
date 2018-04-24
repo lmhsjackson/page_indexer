223064e400b06adef47ac7139915e77a7d9365c5
Jose Piccioni <josepiccioni@gmail.com>
2018-04-24 14:10:43 -0300

    Force encoding to avoid issues in content.

--- app/models/html_tag.rb
+++ app/models/html_tag.rb
@@ -5,7 +5,7 @@ class HtmlTag < ApplicationRecord
 
   def self.build_from(elements)
     elements.map do |e|
-      { name: e.name, content: e.to_s }
+      { name: e.name, content: e.to_s.force_encoding("ISO-8859-1").encode("UTF-8") }
     end
   end
 end
