636975172c20cec154cddd09f7bd009b7932b170
Jose Piccioni <josepiccioni@gmail.com>
2018-04-24 13:35:46 -0300

    Add index and parser methods in the website class.
      Add specs

--- app/models/website.rb
+++ app/models/website.rb
@@ -1,4 +1,13 @@
+require 'html_parser'
 class Website < ApplicationRecord
   has_many :html_tags
   validates :url, presence: true
+
+  def index
+    html_tags << HtmlTag.create_from(parser.parsed_elements)
+  end
+
+  def parser
+    @parser ||= HtmlParser.new(url)
+  end
 end
--- spec/models/website_spec.rb
+++ spec/models/website_spec.rb
@@ -1,5 +1,23 @@
 require 'rails_helper'
+require 'parser_helper'
 
 RSpec.describe Website, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+include ParserHelper
+
+  describe '#parser' do
+    it 'instantiates an HtmlParser object' do
+      url = 'fake url'
+      expect(Website.new(url: url).parser).to be_a(HtmlParser)
+    end
+  end
+
+  describe '#index' do
+    it 'creates the tags from parsed elements' do
+      website = Website.create(url: 'test_url')
+      allow_any_instance_of(HtmlParser).to receive(:parsed_elements)
+        .and_return(parsed_elements)
+      website.index
+      expect(website.html_tags.count).to eq(5)
+    end
+  end
 end
